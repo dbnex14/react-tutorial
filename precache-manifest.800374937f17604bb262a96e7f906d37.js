c65a3a7973ab423fd1ea3b9e6cfe94703a584998
dino buljubasic <dbnex14@gmail.com>
2019-02-25 14:55:06 -0800

    Updates

---
+++ precache-manifest.800374937f17604bb262a96e7f906d37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb5d79de04669f1a9fcf",
+    "url": "/dbnex14/react-tutorial.git/static/css/main.93e9dfbf.chunk.css"
+  },
+  {
+    "revision": "fb5d79de04669f1a9fcf",
+    "url": "/dbnex14/react-tutorial.git/static/js/main.fb5d79de.chunk.js"
+  },
+  {
+    "revision": "98c9f08ec1a76960a352",
+    "url": "/dbnex14/react-tutorial.git/static/js/runtime~main.98c9f08e.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/dbnex14/react-tutorial.git/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "71c1688e1a7dd865a8ca105aa7bff4fa",
+    "url": "/dbnex14/react-tutorial.git/index.html"
+  }
+];
